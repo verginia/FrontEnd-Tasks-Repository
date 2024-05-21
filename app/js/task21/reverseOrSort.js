8be34f6066e8a2bc03144ff61de6cd4510fc8b1a
verginia <geenika@gmail.com>
2024-05-21 10:48:55 +0300

    Added as a comment another solution for the second task with reverse and sort.

--- app/js/task21/reverseOrSort.js
+++ app/js/task21/reverseOrSort.js
@@ -26,3 +26,20 @@ function clearForm() {
     document.getElementById('inputValue').value = '';
     document.getElementById('result').innerText = '';
 } 
+
+/*
+ another solution:
+
+ function processInput(input){
+    if(typeof input === 'number'){
+        // Convert number to string, split into array, join back to string, convert back to number
+        return parseInt(input.toString().split('').reverse().join(''));
+    } else if (typeof input === 'string'){
+        // Split string into array of characters, sort the array, join back to string
+        return input.split('').sort().join('');
+    } else{
+        // If input is neigher a number nor a string, return a message or handle as needed
+        return 'Invalid input type';
+    }
+ }
+ */
